c111ff4e13c2782f3f75f77fc2f02da695fc3c41
tredmond <tredmond@30a32298-b1ee-0310-87e3-a7336bc3aa68>
2012-03-27 18:48:39 +0000

    moving bioportal-services-lib source files over

---
+++ bioportal-services-lib/trunk/src/org/ncbo/stanford/bean/concept/Data.java
@@ -0,0 +1,13 @@
+package org.ncbo.stanford.bean.concept;
+
+public class Data {
+	private ClassBean classBean;
+
+	public void setClassBean(ClassBean classBean) {
+		this.classBean = classBean;
+	}
+
+	public ClassBean getClassBean() {
+		return classBean;
+	}
+}
