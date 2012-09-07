7fab3f3d34c25a313bba668fc7e700b4c0920562
tredmond <tredmond@30a32298-b1ee-0310-87e3-a7336bc3aa68>
2012-09-07 21:44:14 +0000

    added a unit test because I wanted to use the ontology list function

--- bioportal-services-lib/trunk/src.test/org/ncbo/stanford/BioportalOntologyListTest.java
+++ bioportal-services-lib/trunk/src.test/org/ncbo/stanford/BioportalOntologyListTest.java
@@ -7,39 +7,32 @@ import java.util.Iterator;
 import java.util.List;
 
 import org.ncbo.stanford.bean.ontology.OntologyBean;
+import org.ncbo.stanford.util.BioPortalServerConstants;
 import org.ncbo.stanford.util.BioportalOntologyList;
+import org.testng.Assert;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
 
 public class BioportalOntologyListTest {
 	
-	
-	public void test() throws MalformedURLException {
+    
+    @Parameters({"ontologyId", "displayLabel"})
+	@Test
+	public void test(int ontologyId, String displayLabel) throws MalformedURLException {
 		BioportalOntologyList c = new BioportalOntologyList();
 		// String urlStr = "http://rest.bioontology.org/bioportal/ontologies";
-		String urlStr = "http://stagerest.bioontology.org/bioportal/ontologies";
+		String urlStr = "http://stagerest.bioontology.org/bioportal/ontologies" + "?" + BioPortalServerConstants.BP_PRODUCTION_PROTEGE_API_KEY;;
 
 		List<OntologyBean> obList = c.getOntologyProperties(new URL(urlStr));
-		OntologyBean ob;
-		List<String> ontologyList = new ArrayList<String>(0);
-
-		for (Iterator<OntologyBean> it = obList.iterator(); it.hasNext ();) {
-			ob = it.next ();
-			System.out.println(ob.getOntologyId() + " " + ob.getId() + " " + ob.getDisplayLabel());
-			ontologyList.add(ob.getDisplayLabel() + " (" + ob.getId() + ")");
-			boolean isView = ob.isView();
-			System.out.println("Is view: " + isView);
-			if (isView) {
-				System.out.println("\tIs view on ontology version ids: " + ob.getViewOnOntologyVersionId());
-			}
-			List<Integer> hasViews = ob.getHasViews();
-			if ( hasViews != null && !hasViews.isEmpty()) {
-				System.out.println("Has views: ");
-				for (Integer viewId : hasViews) {
-					System.out.println("\t" + viewId);
-				}
-			}
-			else {
-				System.out.println("Has NO views defined!");
-			}
+		Assert.assertTrue(obList.size() > 100);
+		OntologyBean matchedOntology = null;
+		for (OntologyBean ob : obList) {
+		    if (ob.getDisplayLabel().equals(displayLabel)) {
+		        Assert.assertNull(matchedOntology);
+		        Assert.assertEquals((int) ob.getId(), ontologyId);
+		        matchedOntology = ob;
+		    }
 		}
+		Assert.assertNotNull(matchedOntology);
 	}
 }
